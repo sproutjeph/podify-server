628ae395dd2f96b6cb4d2dedce73950809209db2
jephthah <jephthah.mbah@outlook.com>
2023-09-27 15:05:02 +0100

    favorite route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import authRouter from "@/routers/auth";
+import audioRouter from "@/routers/audio";
+import favoriteRouter from "@/routers/favorite";
 
 const app = express();
 
@@ -7,5 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/auth", authRouter);
+app.use("/audio", audioRouter);
+app.use("/favorite", favoriteRouter);
 
 export default app;
